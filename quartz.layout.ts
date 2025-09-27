d0266d26d357072be6fb0b6d52b35348647eb221
Tien Nguyen <tien.nguyew@gmail.com>
2025-09-28 02:31:51 +0700

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -6,12 +6,12 @@ export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
   afterBody: [],
-  // footer: Component.Footer({
-    // links: {
-      // GitHub: "https://github.com/jackyzha0/quartz",
-      // "Discord Community": "https://discord.gg/cRFFHYye7t",
-    // },
-  // }),
+  footer: component.footer({
+    links: {
+      github: "https://github.com/jackyzha0/quartz",
+      "discord community": "https://discord.gg/crffhyye7t",
+    },
+  }),
 }
 
 // components for pages that display a single page (e.g. a single note)
